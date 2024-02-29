44b1ee8d34451ba994f538493c137b9d9c6476ba
Adly <adly@Adly.local>
2024-02-29 16:23:18 +0700

    py

--- ifelse_2.py
+++ ifelse_2.py
@@ -1,8 +1,18 @@
-nilai = 50
+nilai = int(input("NILAI : "))
 
-print('Nilai anda adalah:', nilai, '\n')
+print("Nilain anda :", nilai, "\n")
 
-if nilai >= 70:
-  print('Selamat, anda lulus!')
+if nilai > 100:
+    print("Nilai melebihi 100")
+elif nilai >= 86:
+    print('LULUS A')
+elif nilai >= 75:
+    print('LULUS B')
+elif nilai >= 61:
+    print('TIDAK LULUS C')
+elif nilai >= 46:
+    print('TIDAK LULUS D')
+elif nilai >= 0:
+    print('TIDAK LULUS E')
 else:
-  print('Maaf, anda tidak lulus.')
+    print("Nilai kurang dari 0")
